f6ab358b3f094d14ae15a977c4d69d8bbe1eb776
Marshall Han <beingbook@gmail.com>
2026-02-07 22:26:23 +0900

    setup prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
+
+export default config
